8241397c06115a3b17b11060fc51becd30715c7f
Gilliady <gilliadyazevedo@gmail.com>
2021-01-31 02:11:23 -0300

    no message

---
+++ Assets/scripts/LookAtPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public Transform PlayerT;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        
+    }
+}
